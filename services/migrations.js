da704c4d77fa5c43fd98e014b75e9a2d8fc5faf5
S5 <carlos.diaz@sinco.com.co>
2017-05-25 12:49:46 -0500

    Migracion
    Migracion

--- services/migrations.js
+++ services/migrations.js
@@ -5,7 +5,15 @@ var dropScript = function(req, res, next){
   fs.readFile('scripts/drop.sql', 'utf8', function(err, data_sql) {
     if (err) return next(err);
 
-    db.database().result(data_sql)
+    db.database().tx(t => {
+        var queries = [
+            t.none(data_sql)
+        ];
+        return t.batch(queries);
+    })
+
+
+    //result(data_sql)
     .then(function (data) {
       res.status(200)
         .json({
