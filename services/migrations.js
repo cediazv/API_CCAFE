4a14599a1b980e1ca53472817a87fbeeb86fd7bc
S5 <carlos.diaz@sinco.com.co>
2017-05-25 15:50:49 -0500

    A
    A

--- services/migrations.js
+++ services/migrations.js
@@ -1,28 +1,28 @@
 var db = require('../utilities/database');
 var fs = require('fs');
-var path = require('path');
-
-// Helper for linking to external query files:
-function sql(file) {
-    var fullPath = path.join(__dirname, file);
-    return new db.pgp.QueryFile(fullPath, {minify: true});
-}
-
-var sqlDrop = sql('../scripts/drop.sql');
-
 var dropScript = function(req, res, next){
 
-  db.database().none('DROP TABLE IF EXISTS menu_comite;')
-    .then(data=> {
-      res.status(200)
-        .json({
-          status: 'success',
-          data: data,
-          message: 'Eliminó toda la estructura'
-        });
-    })
-    .catch(error=> {
-        return next(error);
+  fs.readFile('scripts/drop.sql', 'utf8', function(err, data_sql) {
+    if (err) return next(err);
+
+    var sqls = data_sql.split('\n');
+    var c = 0;
+    sqls.forEach(function(s){
+      db.database().none(s)
+      .then(data=> {
+        c++;
+        if (c==sqls.length){
+          res.status(200)
+            .json({
+              status: 'success',
+              data: data,
+              message: 'Eliminó toda la estructura'
+            });
+        }
+      })
+      .catch(error=> {
+          return next(error);
+      });
     });
 }
 
