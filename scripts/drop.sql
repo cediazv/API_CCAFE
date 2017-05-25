768579e5c980dcdd06f268fc3cb3018517c7323d
S5 <carlos.diaz@sinco.com.co>
2017-05-25 15:43:32 -0500

    A
    A

--- scripts/drop.sql
+++ scripts/drop.sql
@@ -1,1 +1,5 @@
-﻿DROP TABLE IF EXISTS menu_comite;DROP TABLE IF EXISTS integrante_comite;DROP TABLE IF EXISTS comite;DROP TABLE IF EXISTS integrante;DROP TABLE IF EXISTS rol;
+﻿DROP TABLE IF EXISTS ´menu_comite´;
+DROP TABLE IF EXISTS ´integrante_comite´;
+DROP TABLE IF EXISTS ´comite´;
+DROP TABLE IF EXISTS ´integrante´;
+DROP TABLE IF EXISTS ´rol´;
