e135d3a846f117956efdf6bb9b141190c379c5e3
S5 <carlos.diaz@sinco.com.co>
2017-05-26 12:21:05 -0500

    A
    A

--- services/menu.js
+++ services/menu.js
@@ -2,7 +2,7 @@ var db = require('../utilities/database');
 
 var getMenu = function(req, res, next){
   var cod = parseInt(req.params.cod);
-  db.database().any('select nombre_menu, url_menu from menu_comite where cod_comite = $1 order by cod_menu', cod)
+  db.database().any('select cod_menu, nombre_menu, url_menu from menu_comite where cod_comite = $1 order by cod_menu', cod)
     .then(function (data) {
       res.status(200)
         .json({
